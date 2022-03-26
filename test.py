495601aad874195d85f4308810903ca3dac053de
hug180 <hug180@my.utsa.edu>
2022-03-26 15:05:48 -0500

    test2 addition

--- test.py
+++ test.py
@@ -5,12 +5,17 @@ pygame.init()
 dis=pygame.display.set_mode((400,300))
 pygame.display.update()
 pygame.display.set_caption('Snake game by Edureka')
+
+blue=(0,0,255)
+red=(255,0,0)
+
 game_over=False
 while not game_over:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             game_over=True
         print(event)   #prints out all the actions that take place on the screen
- 
+    pygame.draw.rect(dis,blue,[200,150,10,10])
+    pygame.display.update()
 pygame.quit()
 quit()
